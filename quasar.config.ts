8f756a535f983da310fb3d3e36c0519b3133a316
tonpor888 <pongskon058@gmail.com>
2025-10-08 09:56:12 +0700

    Edited Notify in quasar.config.js

--- quasar.config.ts
+++ quasar.config.ts
@@ -98,7 +98,9 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: [
+        ['Notify']
+      ],
     },
 
     // animations: 'all', // --- includes all animations
